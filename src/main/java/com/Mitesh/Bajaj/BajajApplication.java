ea45101c78714a0ec61a90433189ca98da75b2b3
miteshmalviya919 <miteshmalviya919@gmail.com>
2025-05-11 16:59:48 +0530

    Update BajajApplication.java

--- src/main/java/com/Mitesh/Bajaj/BajajApplication.java
+++ src/main/java/com/Mitesh/Bajaj/BajajApplication.java
@@ -1,13 +1,22 @@
 package com.Mitesh.Bajaj;
 
+import com.Mitesh.Bajaj.service.WebhookService;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BajajApplication {
+	@Autowired
+	private WebhookService webhookService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(BajajApplication.class, args);
+	}
+	@PostConstruct
+	public void runOnStartup() {
+		webhookService.runChallengeFlow();
 	}
 
 }
